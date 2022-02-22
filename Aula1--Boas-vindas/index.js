b38b1c457059254abce6dd3440a6f1b77b76f083
xk08 <gronytzki@gmail.com>
2022-02-22 08:15:22 -0300

    Arquivos da aula 1

---
+++ Aula1--Boas-vindas/index.js
@@ -0,0 +1,9 @@
+// variável do tipo textual/String
+let nome = "Marcos Martins";
+
+// variável do tipo numerica/inteiro
+var idade = 24;
+
+//Exibindo os dados capturados
+console.log("Olá, eu sou "+nome+ "e tenho "+idade+ " anos.");
+
