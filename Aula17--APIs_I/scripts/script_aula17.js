4028ba4978949f96463b57e58be6f377d1a4ac8f
XK08 <gronytzki@gmail.com>
2022-03-30 23:34:07 -0300

    Aula dia 30-03, APIs 1, arquivos feitos em aula

--- Aula17--APIs_I/scripts/script_aula17.js
+++ Aula17--APIs_I/scripts/script_aula17.js
@@ -6,9 +6,36 @@ botaoPesquisar.addEventListener('click', function (evento) {
     evento.preventDefault();
 
     if (quantidadeFormulario.value != "") {
-        //Executar a busca a API...
 
-        
+        //GET
+        fetch(`https://dog.ceo/api/breeds/image/random/${quantidadeFormulario.value}`) //endpoint da Api
+            .then(function (resultado) {
+
+                //Se a requisição tiver obtido sucesso e tiver o código como 200
+                if (resultado.status == 200) {
+                    //Resolve a promisse do Body.json() e retorna as informações e um objeto JS
+                    return resultado.json();
+                } else {
+                    throw `A requisição não obteve sucesso, retornou o código ${resultado.status}`
+                }
+            })
+            .then(function (resultado) {
+                //Recebe as informações da API e faz a manipulação desejada
+                console.log(resultado.message);
+                
+                // Percorrendo os objetos no array de cachorros
+                for (let img of resultado.message) {
+                    //Exibe as fotos dos cachorros em tela
+                    let imgelemento = document.createElement('img');
+                    imgelemento.src = img;
+                    exibeResultado.appendChild(imgelemento);
+                }
+            })
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                }
+            );
     } else {
         alert("Você deve informar o número de imagens que deseja buscar")
     }
